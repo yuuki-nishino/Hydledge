59a50fa7cf96bc5cbfb111f94e8a88ad007aad9a
yuuki-nishino <yuukinishino@d46.private>
2022-04-06 20:55:21 +0900

    install bootstrap

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -4,6 +4,7 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+  <button class="btn btn-primary">bootstrap</button>
 </template>
 
 <style lang="scss">
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
 createApp(App).use(store).use(router).mount('#app')
